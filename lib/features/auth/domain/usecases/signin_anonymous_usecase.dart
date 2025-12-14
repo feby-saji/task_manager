9a7063037dd4597839b71ade1b1a184eb3996270
feby-saji <febysaji621@gmail.com>
2025-12-14 09:27:07 +0530

    feat(auth) refactored sign in anonymous

--- lib/features/auth/domain/usecases/signin_anonymous_usecase.dart
+++ lib/features/auth/domain/usecases/signin_anonymous_usecase.dart
@@ -1,14 +1,8 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:task_manager/features/auth/domain/entities/user_entity.dart';
 import 'package:task_manager/features/auth/domain/repositories/auth_repository.dart';
 
 import '../../../../core/utils/failures.dart';
-
-final signInAnonymousUseCaseProvider = Provider<SignInAnonymousUseCase>((ref) {
-  final authRepository = ref.read(authRepositoryProvider);
-  return SignInAnonymousUseCase(authRepository);
-});
 
 class SignInAnonymousUseCase {
   final AuthRepository _authRepository;
