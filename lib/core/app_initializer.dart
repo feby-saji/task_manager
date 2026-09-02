49d7ae2473323a283de0f5c7af18f7586ebb2c7c
feby-saji <febysaji621@gmail.com>
2026-09-02 09:20:08 +0530

    fix: resolve Flutter build environment issues (JDK, Kotlin, Gradle)

--- lib/core/app_initializer.dart
+++ lib/core/app_initializer.dart
@@ -1,7 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:task_manager/secrets.dart';
+import 'app_secrets.dart';
 
 class AppInitializer {
   static Future<void> initialize() async {
@@ -10,7 +10,7 @@ class AppInitializer {
     // Supabase
     await Supabase.initialize(
       url: AppSecrets.supabaseUrl,
-      anonKey: AppSecrets.anonKey,
+      anonKey: AppSecrets.publishableKey,
     );
 
     // Any other plugin
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
   await AppInitializer.initialize();
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: false,
       tools: const [
         ...DevicePreview.defaultTools,
         // CustomPlugin(),
